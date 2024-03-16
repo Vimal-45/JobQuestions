58d9ef58d5066dbee7e89a8fd9a4b8c29a471b52
Vimal-45 <dvimal45@gmail.com>
2024-03-16 11:19:17 -0400

    handlesubmit change

--- src/Commponents/Login.jsx
+++ src/Commponents/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
             </button>
 
             <div className="w-full flex-1 mt-8">
-              <form className="space-y-4" onSubmit={() => handleSubmit}>
+              <form className="space-y-4" onSubmit={handleSubmit}>
                 <div className="flex flex-col items-center">
                   <input
                     className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
